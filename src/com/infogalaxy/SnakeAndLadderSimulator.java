169a2d692e9ca4578a109750dfd1943b818ee81f
Prakash7277 <pkpatil.366@gmail.com>
2022-04-07 10:25:49 +0530

    MODIFIED:Player Exact Winning Position 100

--- src/com/infogalaxy/SnakeAndLadderSimulator.java
+++ src/com/infogalaxy/SnakeAndLadderSimulator.java
@@ -19,6 +19,7 @@ public class SnakeAndLadderSimulator {
     //Rolling the die to get Position
     public void rollsDie() {
         while (position!=100) {
+            System.out.println("************************************");
             Random random = new Random();
             int dieNo = (int) ((Math.random() * (7 - 1)) + 1);
             System.out.println("Die Number : " + dieNo);
@@ -30,15 +31,31 @@ public class SnakeAndLadderSimulator {
                 case NO_PLAY:
                     System.out.println("No play Arrived. Stay on same Position");
                     break;
-                case LADDER:
-                    System.out.println("HURRAY !!! you got ladder.");
-                    position = position + dieNo;
-                    break;
-                case SNAKE:
-                    System.out.println("OOPS  !!! you got Snake. ");
-                    position = position - dieNo;
-                    break;
+                        case LADDER:
+                            System.out.println("HURRAY !!! you got ladder.");
+                            position = position + dieNo;
+                            if (position > 100) {
+                                System.out.println("Player Position is Out of Board.");
+                                position = position - dieNo;
+                            }
+                            break;
+                        case SNAKE:
+                            position = position + dieNo;
+                            if (position <= 100) {
+                                System.out.println("OOPS  !!! you got Snake. ");
+                                position = position - dieNo - dieNo;
+                                if (position < 0) {
+                                    position = 0;
+                                    break;
+                                }
+                            } else {
+                                System.out.println("Player Position is Out of Board.");
+                                position = position - dieNo;
+                            }
+                            break;
+
             }
+            showPosition();
         }
     }
 
